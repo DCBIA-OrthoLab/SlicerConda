bb0df3c46e796a05d37342b850ab9a5d524e0161
Leroux Gaelle <gaelleleroux974@gmail.com>
2024-02-12 10:04:19 -0500

    STYLE : add print

--- Example/Example.py
+++ Example/Example.py
@@ -266,6 +266,7 @@ class ExampleWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
         """Run processing when user clicks "Apply" button."""
         conda = CondaSetUpCall() # Creation of the object
         path_conda = conda.getCondaPath() # Get the conda path to find out if the user has entered it
+        print("path_conda : ",path_conda)
         if path_conda == "None":
           slicer.util.infoDisplay("Path to conda is no set up. Open the module SlicerConda to do it",windowTitle="Can't found conda path")
         else :
@@ -382,36 +383,37 @@ class ExampleLogic(ScriptedLoadableModuleLogic):
                 invert: bool = False,
                 showResult: bool = True) -> None:
         """
-        Run the processing algorithm.
-        Can be used without GUI widget.
-        :param inputVolume: volume to be thresholded
-        :param outputVolume: thresholding result
-        :param imageThreshold: values above/below this threshold will be set to 0
-        :param invert: if True then values above the threshold will be set to 0, otherwise values below are set to 0
-        :param showResult: show output volume in slice viewers
-        """
+        # Run the processing algorithm.
+        # Can be used without GUI widget.
+        # :param inputVolume: volume to be thresholded
+        # :param outputVolume: thresholding result
+        # :param imageThreshold: values above/below this threshold will be set to 0
+        # :param invert: if True then values above the threshold will be set to 0, otherwise values below are set to 0
+        # :param showResult: show output volume in slice viewers
+        # """
 
-        if not inputVolume or not outputVolume:
-            raise ValueError("Input or output volume is invalid")
+        # if not inputVolume or not outputVolume:
+        #     raise ValueError("Input or output volume is invalid")
 
-        import time
+        # import time
 
-        startTime = time.time()
-        logging.info("Processing started")
+        # startTime = time.time()
+        # logging.info("Processing started")
 
-        # Compute the thresholded output volume using the "Threshold Scalar Volume" CLI module
-        cliParams = {
-            "InputVolume": inputVolume.GetID(),
-            "OutputVolume": outputVolume.GetID(),
-            "ThresholdValue": imageThreshold,
-            "ThresholdType": "Above" if invert else "Below",
-        }
-        cliNode = slicer.cli.run(slicer.modules.thresholdscalarvolume, None, cliParams, wait_for_completion=True, update_display=showResult)
-        # We don't need the CLI module node anymore, remove it to not clutter the scene with it
-        slicer.mrmlScene.RemoveNode(cliNode)
+        # # Compute the thresholded output volume using the "Threshold Scalar Volume" CLI module
+        # cliParams = {
+        #     "InputVolume": inputVolume.GetID(),
+        #     "OutputVolume": outputVolume.GetID(),
+        #     "ThresholdValue": imageThreshold,
+        #     "ThresholdType": "Above" if invert else "Below",
+        # }
+        # cliNode = slicer.cli.run(slicer.modules.thresholdscalarvolume, None, cliParams, wait_for_completion=True, update_display=showResult)
+        # # We don't need the CLI module node anymore, remove it to not clutter the scene with it
+        # slicer.mrmlScene.RemoveNode(cliNode)
 
-        stopTime = time.time()
-        logging.info(f"Processing completed in {stopTime-startTime:.2f} seconds")
+        # stopTime = time.time()
+        # logging.info(f"Processing completed in {stopTime-startTime:.2f} seconds")
+        print("There is no pocess here in this module")
 
 
 #
@@ -451,33 +453,33 @@ class ExampleTest(ScriptedLoadableModuleTest):
 
         # Get/create input data
 
-        import SampleData
+        # import SampleData
 
-        registerSampleData()
-        inputVolume = SampleData.downloadSample("Example1")
-        self.delayDisplay("Loaded test data set")
+        # registerSampleData()
+        # inputVolume = SampleData.downloadSample("Example1")
+        # self.delayDisplay("Loaded test data set")
 
-        inputScalarRange = inputVolume.GetImageData().GetScalarRange()
-        self.assertEqual(inputScalarRange[0], 0)
-        self.assertEqual(inputScalarRange[1], 695)
+        # inputScalarRange = inputVolume.GetImageData().GetScalarRange()
+        # self.assertEqual(inputScalarRange[0], 0)
+        # self.assertEqual(inputScalarRange[1], 695)
 
-        outputVolume = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLScalarVolumeNode")
-        threshold = 100
+        # outputVolume = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLScalarVolumeNode")
+        # threshold = 100
 
-        # Test the module logic
+        # # Test the module logic
 
-        logic = ExampleLogic()
+        # logic = ExampleLogic()
 
-        # Test algorithm with non-inverted threshold
-        logic.process(inputVolume, outputVolume, threshold, True)
-        outputScalarRange = outputVolume.GetImageData().GetScalarRange()
-        self.assertEqual(outputScalarRange[0], inputScalarRange[0])
-        self.assertEqual(outputScalarRange[1], threshold)
+        # # Test algorithm with non-inverted threshold
+        # logic.process(inputVolume, outputVolume, threshold, True)
+        # outputScalarRange = outputVolume.GetImageData().GetScalarRange()
+        # self.assertEqual(outputScalarRange[0], inputScalarRange[0])
+        # self.assertEqual(outputScalarRange[1], threshold)
 
-        # Test algorithm with inverted threshold
-        logic.process(inputVolume, outputVolume, threshold, False)
-        outputScalarRange = outputVolume.GetImageData().GetScalarRange()
-        self.assertEqual(outputScalarRange[0], inputScalarRange[0])
-        self.assertEqual(outputScalarRange[1], inputScalarRange[1])
+        # # Test algorithm with inverted threshold
+        # logic.process(inputVolume, outputVolume, threshold, False)
+        # outputScalarRange = outputVolume.GetImageData().GetScalarRange()
+        # self.assertEqual(outputScalarRange[0], inputScalarRange[0])
+        # self.assertEqual(outputScalarRange[1], inputScalarRange[1])
 
         self.delayDisplay("Test passed")
